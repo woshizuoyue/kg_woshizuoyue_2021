79543aa85872e5e5b153cbedfd1659fc0115be5d
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-03-18 21:16:37 -0400

    second edit

--- src/Main.java
+++ src/Main.java
@@ -8,37 +8,21 @@ public class Main {
 
     public static boolean helper(String args1, String args2){
 
-        if(args1.length() != args2.length()) return false;
-
-        Map<Character, Character> map = new HashMap<>();
+        if(args1.length() == 0 || args1.length() != args2.length()) return false;
 
         Set<Character> set = new HashSet<>();
 
-        char[] chars1 = args1.toCharArray();
-        char[] chars2 = args2.toCharArray();
+        char[] chars = args1.toCharArray();
 
-        Arrays.sort(chars1);
-        Arrays.sort(chars2);
-
-        for(char c : chars1){
+        for(char c : chars){
 
             if(set.contains(c)) return false;
-        }
 
-        for(int i = 0; i<chars1.length; i++){
-
-            if(map.containsKey(chars1[i])){
-
-                if(map.get(chars1[i]) != chars2[i]) return false;
-            }
-
-            else{
-
-                map.put(chars1[i], chars2[i]);
-            }
+            set.add(c);
         }
 
         return true;
+
     }
 }
 
